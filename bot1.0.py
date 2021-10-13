652eb5ea5b2df33bf652a754f9093fb8af4cabdd
dimaskorp <52814195+dimaskorp@users.noreply.github.com>
2021-10-13 16:29:25 +0400

    Update bot1.0.py

--- bot1.0.py
+++ bot1.0.py
@@ -1,70 +1,66 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from aiogram.types import ParseMode
-import requests
-from bs4 import BeautifulSoup as BS
-import lxml
-import json
+from parsing import dep_slovar, budget_slovar, sp, spisok_fio
+import keyboards as kb
+import re
 
 TOKEN = '1552413047:AAGpkNonW31wDJOY7-DnMRCl4iI9eElxUkw'
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-
-
-def get_html():
-    sp = []
-    fio = []
-    HEADERS = {
-        "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Mobile Safari/537.36"
-    }
-    url = "https://igis.ru/online?obj=71&page=zapdoc"
-    response = requests.get(url=url, headers=HEADERS)
-    soup = BS(response.text, "lxml")
-    table = soup.find('table', attrs={'class': 'table-border'})
-    tr = table.find_all('tr')
-    i = 0
-    j = 0
-    for quote in tr:
-        a = len(sp)
-        if quote.attrs == {'class': ['table-border-light']}:
-            if i < a:
-                i += 1
-                sp.append(quote.text.strip())
-                fio.append([])
-                j += 1
-            else:
-                sp.append(quote.text.strip())
-                fio.append([])
-        else:
-            quote_text = quote.text.strip()
-            for link in quote.select('a', href=True):
-                quote_href = 'https://igis.ru/online' + link['href']
-
-            sp.append(sp[i])
-            word = 'Всего номерков'
-            if word in quote.text.strip():
-                fio[j].append(quote_text + ' ' + quote_href)
-            i += 1
-    key = list(dict.fromkeys(sp))
-    for i, elem in enumerate(fio):
-        if not fio[i]:
-            fio[i].append('Номерков нет')
-
-    # special_slovar = dict(zip(key, fio))
-    # with open('MAKE_AN_APPOINTMENT.json', 'w') as file:
-    #     json.dump(special_slovar, file, indent=4, ensure_ascii=False)
-    return key, fio
+data_1 = []
+data_2 = []
+data_3 = []
 
 
 # кнопки вызывающие действия
 @dp.message_handler(commands=['start'])
-async def send_welcome(msg: types.Message):
-    await msg.answer(f'*РЕСПУБЛИКАНСКАЯ СТОМАТОЛОГИЧЕСКАЯ ПОЛИКЛИНИКА* Запись к специалистам',
-                     parse_mode=ParseMode.MARKDOWN)
-    rez = get_html()
-    for i, elem in enumerate(rez[0]):
-        await msg.answer(f'*{elem}*\n\n' + '\n\n'.join(rez[1][i]), parse_mode=ParseMode.MARKDOWN
+async def start(msg: types.Message):
+    await bot.send_message(msg.from_user.id, 'Привет {0.first_name}!\nВыберите раздел:'.format(msg.from_user), reply_markup=kb.keyboard_1())
+
+
+@dp.callback_query_handler(lambda call: call.data == 'keyboard')
+async def callback_inline_1(call: types.CallbackQuery):
+    await bot.delete_message(call.from_user.id, call.message.message_id)
+    await call.message.answer("Выберите раздел:", reply_markup=kb.keyboard_1())
+
+
+@dp.callback_query_handler(lambda call: call.data in kb.spisok_key_1)
+async def callback_inline_2(call: types.CallbackQuery):
+    await bot.delete_message(call.from_user.id, call.message.message_id)
+    data_1.append(call.data)
+    for i, elem in enumerate(dep_slovar):
+        if call.data == kb.spisok_key_1[i]:
+            number = int("".join(re.findall("\d+", call.data)))
+            keys = list(dep_slovar.keys())[i]
+            await call.message.answer(f'{keys}\n\nВыберите учреждение:', reply_markup=kb.keyboard_2(number))
+    kb.spisok_key_1.clear()
+
+
+@dp.callback_query_handler(lambda call: call.data in kb.spisok_key_2)
+async def callback_inline_3(call: types.CallbackQuery):
+    await bot.delete_message(call.from_user.id, call.message.message_id)
+    data_2.append(call.data)
+    for i, elem in enumerate(budget_slovar):
+        if call.data == kb.spisok_key_2[i]:
+            number = int("".join(re.findall("\d+", call.data)))
+            keys = list(budget_slovar.keys())[i]
+            await call.message.answer(f'{keys}\n\nВыберите специализацию:', reply_markup=kb.keyboard_3(number))
+    kb.spisok_key_2.clear()
+
+
+@dp.callback_query_handler(lambda call: call.data in kb.spisok_key_3)
+async def callback_inline_4(call: types.CallbackQuery):
+    await bot.delete_message(call.from_user.id, call.message.message_id)
+    data_3.append(call.data)
+    for i, elem in enumerate(kb.spisok_key_3):
+        if call.data == kb.spisok_key_3[i]:
+            next_menu = types.InlineKeyboardMarkup()
+            back = types.InlineKeyboardButton(text='Назад', callback_data='keyboard')
+            next_menu.add(back)
+            key_ = list(dict.fromkeys(sp))
+            await call.message.answer(f'{key_[i]}!\n\n' + '\n\n'.join(spisok_fio[i]), reply_markup=next_menu)
+    kb.spisok_key_3.clear()
 
 
 if __name__ == '__main__':
